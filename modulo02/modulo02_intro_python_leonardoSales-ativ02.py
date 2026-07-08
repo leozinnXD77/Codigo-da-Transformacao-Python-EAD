6b297f2ac5d0c7a02b771cb1f9d451ef2d31f720
mateussantosrodrigues650-hash <leozinnxd00@gmail.com>
2026-07-08 15:40:19 -0300

    update_progresso
    aprendi a usar o type() para identificar dados e a criar um mini-programa de novo usuário.

---
+++ modulo02/modulo02_intro_python_leonardoSales-ativ02.py
@@ -0,0 +1,10 @@
+print("\n", "-" * 35)
+
+print("olá, mundo!")
+print("vamos ver como funciona o type!")
+nome = "Leonardo"
+idade = 15
+print(type(nome))
+print(type(idade))
+
+print("-" * 35, "\n")
---
+++ modulo02/modulo02_intro_python_leonardoSales-ativ03.py
@@ -0,0 +1,9 @@
+print("\n", "-" * 35)
+
+print("\nolá, usuario!","\n")
+
+nome = input("digite seu nome: ")
+
+print(f"\nolá, {nome}! seja bem-vindo(a) ao meu programa!\n")
+
+print("-" * 35, "\n")
